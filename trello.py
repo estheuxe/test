ef01c3eb513b7d3720e99d022609cd0c40cdca26
estheuxe <siberian.w7y@gmail.com>
2019-09-19 13:22:06 +0700

    post put delete complete

--- trello.py
+++ trello.py
@@ -1,23 +1,116 @@
 import requests
 import json
 
-def postCard():
+key = '27138010bc3a442737533781e5029962'
+token = '0ab806b21beb8db46ff186fb60c364843b541b100249ba7d36cc41f35472ca93'
+oauth = 'c3e49fbf1c8211e11e4ce749e074b0d30cfb25ed1acfd632fcb658c5538d52ef'
+
+url = 'https://api.trello.com/'
+
+# url for getting lists
+urlB = 'https://api.trello.com/1/boards/{id}/lists'
+
+# url for getting cards 
+urlL = 'https://api.trello.com/1/lists/{id}/cards'
+
+# url for work with cards
+urlCards = 'https://api.trello.com/1/cards/{id}'
+
+# url for getting all boards
+getString = 'https://api.trello.com/1/members/me/boards'
+
+def postCard(idList):
+	postUrl = 'https://api.trello.com/1/cards'
+
 	cardName = str(input("Name: "))
 	cardDesc = str(input("Desc: "))
 
-	postQuery = {
+	postCardQuery = {
 		'idList': idList,
 		'name': cardName,
 		'desc': cardDesc,
-		'pos': 'bottom',
 		'key': key,
 		'token': token
 	}
 
-	respCreate = requests.request("POST", urlC, params=postQuery)
-	
-	print("\n***** created *****\n\n")
+	# and finally POST request
+	requests.request("POST", postUrl, params=postCardQuery)
 
+def putCard(idList):
+	cardName = str(input("Name: "))
+
+	# getting all card for names and ids (to find id)
+	respL = requests.request("GET", urlL.format(id=idList), params=queryString)
+
+	# getting ID of card
+	for i in respL.json():
+		if i['name'] == cardName:
+			idCard = i['id']
+
+	newCardName = str(input("New name: "))
+	newCardDesc = str(input("New desc: "))
+
+	putCardQuery = {
+		'name': newCardName,
+		'desc': newCardDesc,
+		'key': key,
+		'token': token
+	}
+
+	# and finally PUT requests
+	requests.request("PUT", urlCards.format(id=idCard), params=putCardQuery)
+
+def deleteCard(idList):
+	cardName = str(input("Name: "))
+
+	# getting all card for names and ids
+	respL = requests.request("GET", urlL.format(id=idList), params=queryString)
+
+	for i in respL.json():
+		if i['name'] == cardName:
+			idCard = i['id']		
+
+	# and finally DELETE request
+	requests.request("DELETE", urlCards.format(id=idCard), params={'key':key,'token':token})
+
+if __name__ == "__main__":
+
+	queryString = {
+		'fields': 'id,name',
+		'key': key,
+		'token': token
+	}
+
+	print("Boards:")
+
+	# getting all boards 
+	response = requests.request("GET", getString, params=queryString)
+
+	# print all boards
+	for i in response.json():
+		print(">>> " + i.get('name'))
+
+	boardName = str(input("\nEnter name of board: "))
+
+	for i in response.json():
+		if i['name'] == boardName:
+			idBoard = i['id']
+
+	print("\nLists of board \"{boardName}\":".format(boardName=boardName))
+
+	# getting lists of chosen board
+	respB = requests.request("GET", urlB.format(id=idBoard), params=queryString)
+
+	for i in respB.json():
+		print(">>> " + i.get('name'))
+
+	listName = str(input("\nEnter name of list: "))
+
+	for i in respB.json():
+		if i['name'] == listName:
+			idList = i['id']
+
+	# getting cards of chosen list
 	respL = requests.request("GET", urlL.format(id=idList), params=queryString)
 
 	print("\nCards of list \"{listName}\":".format(listName=listName))
@@ -25,97 +118,16 @@ def postCard():
 	for i in respL.json():
 		print(">>> " + i.get('name'))
 
-def deleteCard():
-	delUrl = 'https://api.trello.com/1/cards/{id}'
-
-	cardName = str(input("Name: "))
-
-	deleteQuery = {
-		'id': idCard,
-		'key': key,
-		'token': token
-	}
-
-	respDel = requests.request("DELETE", delUrl, params=deleteQuery)
-
-	for i in respB.json():
-		if i['name'] == listName:
-			idList = i['id']
-
-
-
-key = '27138010bc3a442737533781e5029962'
-token = '0ab806b21beb8db46ff186fb60c364843b541b100249ba7d36cc41f35472ca93'
-oauth = 'c3e49fbf1c8211e11e4ce749e074b0d30cfb25ed1acfd632fcb658c5538d52ef'
-
-url = 'https://api.trello.com/'
-urlB = 'https://api.trello.com/1/boards/{id}/lists'
-urlL = 'https://api.trello.com/1/lists/{id}/cards'
-urlC = 'https://api.trello.com/1/cards'
-
-queryString = {
-	'fields': 'id,name',
-	'key': key,
-	'token': token
-}
-
-#idBoard = '5d81c5e6ecf65d36ef777b70'	# board "MyBoard"
-#idList = '5d81c5e68f079e461725ca0b'		# list  "In progress"
-
-getString = 'https://api.trello.com/1/members/me/boards'
-
-getActions = 'https://api.trello.com/1/boards/{idBoard}/actions'
-
-#response = requests.request("GET", getActions.format(boardId=idBoard), params=querystring)
-
-print("Boards:")
-
-# getting all boards 
-response = requests.request("GET", getString, params=queryString)
-
-for i in response.json():
-	print(">>> " + i.get('name'))
-
-boardName = str(input("\nEnter name of board: "))
-
-for i in response.json():
-	if i['name'] == boardName:
-		idBoard = i['id']
-
-boardName = 'MyBoard'
-
-print("\nLists of board \"{boardName}\":".format(boardName=boardName))
-
-# getting lists of board
-respB = requests.request("GET", urlB.format(id=idBoard), params=queryString)
-
-for i in respB.json():
-	print(">>> " + i.get('name'))
-
-listName = str(input("\nEnter name of list: "))
-
-for i in respB.json():
-	if i['name'] == listName:
-		idList = i['id']
-
-# getting cards of list
-respL = requests.request("GET", urlL.format(id=idList), params=queryString)
-
-print("\nCards of list \"{listName}\":".format(listName=listName))
-
-for i in respL.json():
-	print(">>> " + i.get('name'))
-
-action = str(input("\nWhat would you like to do? (First word)\n\
+	action = str(input("\nWhat would you like to do? (First word)\n\
 >>> Create new card\n\
 >>> Edit existing card\n\
 >>> Delete card\n"))
 
-if action == "Create":
-	postCard()
-elif action == "Edit":
-	print("edit")
-elif action == "Delete":
-	deleteCard()
-else:
-	print("default")
+	if action == "Create":
+		postCard(idList)
+	elif action == "Edit":
+		putCard(idList)
+	elif action == "Delete":
+		deleteCard(idList)
+	else:
+		print("wrong command")
