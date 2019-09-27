3d0962f6d1d39ded13ad61ee6c287a85e6e512bc
estheuxe <siberian.w7y@gmail.com>
2019-09-27 14:00:17 +0700

    invaderrr

--- OOP/oopforte.py
+++ OOP/oopforte.py
@@ -1,61 +1,32 @@
+import requests
+
 class Taskedit():
 	def __init__(self, type):
 		self.type = type
 
 	def __getType(self, type):
-		self.type = type
-		if self.type == 'trello':
+		if type == 'trello':
 			return Trello()
-		elif self.type == 'yt':
+		elif type == 'yt':
 			return Yt()
 
 	def watchBoards(self):
-		tp = self.__getType(self.type)
+		return self.__getType(self.type).watchBoards()
 
 	def watchLists(self, idBoard):
-	def watchCards(self, idList):
-		def createCard(self, idList, cardName, cardDesc):
-		def updateCard(self, idCard, newCardName, newCardDesc):
-		def removeCard(self, idCard):
+		return self.__getType(self.type).watchLists(self, idBoard)
 
-	def createCard(self, cardName, cardDesc):
-		self.cardName = cardName
-		self.cardDesc = cardDesc
+	def watchCards(self, idList):
+		return self.__getType(self.type).watchCards(self, idList)
+
+	def createCard(self, idList, cardName, cardDesc):
+		return self.__getType(self.type).createCard(self, idList, cardName, cardDesc)
 
 	def updateCard(self, idCard, newCardName, newCardDesc):
-		self.idCard = idCard
-		self.newCardName = newCardName
-		self.newCardDesc = newCardDesc
+		return self.__getType(self.type).updateCard(self, idCard, newCardName, newCardDesc)
 
 	def removeCard(self, idCard):
-		self.idCard = idCard
-
-	def watchResponse(self, response) -> 'Response':
-		self.response = response
-		if self.response.status_code == 200:
-			return Response(self.response.json(), status=status.HTTP_200_OK)
-		else:
-			return Response(self.response.status_code)
-
-	def createResponse(self, response) -> 'Response':
-		self.response = response
-		if self.response.status_code == 200:
-			return Response(status=status.HTTP_201_CREATED)
-		else:
-			return Response(self.response.status_code)
-
-	def updateResponse(self, response) -> 'Response':
-		self.response = response
-		if self.response.status_code == 200:
-			return Response(status=status.HTTP_202_ACCEPTED)
-		else:
-			return Response(self.response.status_code)
-
-	def deleteResponse(self, response) -> 'Response':
-		if response.status_code == 200:
-			return Response(status=status.HTTP_204_NO_CONTENT)
-		else:
-			return Response(response.status_code)
+		return self.__getType(self.type).updateCard(self, idCard)
 
 class Trello(Taskedit):
 	trelloQs = {
@@ -65,27 +36,20 @@ class Trello(Taskedit):
 	}
 
 	def watchBoards(self):
-		response = requests.request('GET', s.URL_BOARDS, params=Trello.trelloQS)
-		rsp = Taskedit.watchResponse(self, response)
-		return rsp
+		return requests.request('GET', s.URL_BOARDS, params=Trello.trelloQS)
 
 	def watchLists(self, idBoard):
 		self.idBoard = idBoard
-		#idBoard = request.GET.get('id')
-		response = requests.request('GET', s.URL_LISTS.format(id=self.idBoard), params=Trello.trelloQS)
-		rsp = Taskedit.watchResponse(self, response)
-		return rsp
+		return requests.request('GET', s.URL_LISTS.format(id=self.idBoard), params=Trello.trelloQS)
 
 	def watchCards(self, idList):
 		self.idList = idList
-		#idList = request.GET.get('id')
-		response = requests.request('GET', s.URL_CARDS.format(id=self.idList), params=Trello.trelloQS)
-		rsp = Taskedit.watchResponse(self, response)
-		return rsp
+		return requests.request('GET', s.URL_CARDS.format(id=self.idList), params=Trello.trelloQS)
 
 	def createCard(self, idList, cardName, cardDesc):
 		self.idList = idList
-		Taskedit.createCard(self, cardName, cardDesc)
+		self.cardName = cardName
+		self.cardDesc = cardDesc
 		postTrelloCardQuery = {
 			'idList': self.idList,
 			'name': self.cardName,
@@ -93,29 +57,23 @@ class Trello(Taskedit):
 			'key': s.TRELLO_KEY,
 			'token': s.TRELLO_TOKEN
 		}
-
-		response = requests.request('POST', s.POST_TRELLO_URL, params=postTrelloCardQuery)
-		rsp = Taskedit.createResponse(self, response)
-		return rsp
+		return requests.request('POST', s.POST_TRELLO_URL, params=postTrelloCardQuery)
 
 	def updateCard(self, idCard, newCardName, newCardDesc):
-		Taskedit.updateCard(self, idCard, newCardName, newCardDesc)
+		self.idCard = idCard
+		self.newCardName = newCardName
+		self.newCardDesc = newCardDesc
 		putTrelloCardQuery = {
 			'name': self.newCardName,
 			'desc': self.newCardDesc,
 			'key': s.TRELLO_KEY,
 			'token': s.TRELLO_TOKEN
 		}
-
-		response = requests.request('PUT', s.URL_FOR_CARD.format(id=self.idCard), params=putTrelloCardQuery)
-		rsp = Taskedit.updateResponse(self, response)
-		return rsp
+		return requests.request('PUT', s.URL_FOR_CARD.format(id=self.idCard), params=putTrelloCardQuery)
 
 	def removeCard(self, idCard):
-		Taskedit.removeCard(self, idCard)
-		response = requests.request('DELETE', s.URL_FOR_CARD.format(id=self.idCard), params={'key': s.TRELLO_KEY,'token': s.TRELLO_TOKEN})
-		rsp = Taskedit.deleteResponse(self, response)
-		return rsp
+		self.idCard = idCard
+		return requests.request('DELETE', s.URL_FOR_CARD.format(id=self.idCard), params={'key': s.TRELLO_KEY,'token': s.TRELLO_TOKEN})
 
 class Yt(Taskedit):
 	ytQs = {
@@ -123,21 +81,18 @@ class Yt(Taskedit):
 	}
 
 	def watchBoards(self):
-		response = requests.request('GET', s.YT_URL_BOARDS, params=Yt.ytQs, headers=s.YT_HEADERS)
-		rsp = Taskedit.watchResponse(self, response)
-		return rsp
+		return requests.request('GET', s.YT_URL_BOARDS, params=Yt.ytQs, headers=s.YT_HEADERS)
 
 	def watchCards(self):
 		ytWatchCardFields = {
 			'fields': 'id,summary,name,description,reporter(login)'
 		}	
-		response = requests.request('GET', s.YT_URL_CARDS, params=ytWatchCardFields, headers=s.YT_HEADERS)
-		rsp = Taskedit.watchResponse(self, response)
-		return rsp
+		return requests.request('GET', s.YT_URL_CARDS, params=ytWatchCardFields, headers=s.YT_HEADERS)
 
 	def createCard(self, idBoard, cardName, cardDesc):
 		self.idBoard = idBoard
-		Taskedit.createCard(self, cardName, cardDesc)
+		self.cardName = cardName
+		self.cardDesc = cardDesc
 		ytCreateCardFields = {
 			'fields': 'idReadable'
 		}
@@ -148,13 +103,10 @@ class Yt(Taskedit):
 				'id': self.idBoard,
 			}
 		}
-
-		response = requests.request('POST', s.YT_URL_CARDS, params=ytCreateCardFields, headers=s.YT_HEADERS, json=ytJson)			
-		rsp = Taskedit.createResponse(self, response)
-		return rsp
+		return requests.request('POST', s.YT_URL_CARDS, params=ytCreateCardFields, headers=s.YT_HEADERS, json=ytJson)			
 
 	def updateCard(self, idCard, newCardName, newCardDesc):
-		Taskedit.updateCard(self, idCard, newCardName, newCardDesc)
+		self.idCard = idCard
 		self.newCardName = newCardName
 		self.newCardDesc = newCardDesc
 		ytJson = {
@@ -162,12 +114,8 @@ class Yt(Taskedit):
 			'description': self.newCardDesc
 		}
 		# у них почему-то на изменение POST request
-		response = requests.request('POST', s.YT_URL_CARD_EDIT.format(id=self.idCard), headers=s.YT_HEADERS, json=ytJson)
-		rsp = Taskedit.updateResponse(self, response)
-		return rsp
+		return requests.request('POST', s.YT_URL_CARD_EDIT.format(id=self.idCard), headers=s.YT_HEADERS, json=ytJson)
 
 	def removeCard(self, idCard):
-		Taskedit.removeCard(self, idCard)
-		response = requests.request('DELETE', s.YT_URL_CARD_EDIT.format(id=self.idCard), headers=s.YT_HEADERS)
-		rsp = Taskedit.deleteResponse(self, response)
-		return rsp
+		self.idCard = idCard
+		return requests.request('DELETE', s.YT_URL_CARD_EDIT.format(id=self.idCard), headers=s.YT_HEADERS)
